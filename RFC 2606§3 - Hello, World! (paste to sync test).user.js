cbcc6493c7d5646d5278d8fc20063154633799d2
Mike Medley <medleymind@gmail.com>
2014-06-24 16:07:01 -0500

    Create RFC 2606§3 - Hello, World! (paste to sync test).user.js

---
+++ RFC 2606§3 - Hello, World! (paste to sync test).user.js
@@ -0,0 +1,27 @@
+(function () {
+
+// ==UserScript==
+// @name          RFC 2606§3 - @license and @licence (recovered) Unit Test
+// @namespace     http://localhost.localdomain
+// @description   Tests out non-unique keys
+// @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
+// @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @licence       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
+// @version       2014.06.22.1
+// @icon          https://s3.amazonaws.com/uso_ss/icon/13701/large.png
+
+// @homepageURL  http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @homepageURL  https://openuserjs.org/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+
+// @supportURL   http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_(recovered)_Unit_Test/issues
+// @supportURL   mailto:user@example.com
+
+// @include   http://www.example.com/*
+// @include   http://www.example.net/*
+// @include   http://www.example.org/*
+
+// ==/UserScript==
+
+  // Do Nothing
+
+})();
