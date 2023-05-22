e16d7507e188c42f50ac1e00f4541eb707229bc5
sizzlemctwizzle <medleymind@gmail.com>
2023-05-22 15:48:40 -0500

    Add a new script to reveal password fields.

---
+++ Show_All_Passwords.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name          Show All Passwords
+// @namespace     sizzlemctwizzle
+// @description   Show all passwords as plain-text. Use at your own risk!
+// @copyright     2023, sizzle (https://openuserjs.org/users/sizzle)
+// @license       MIT
+// @version       1.0
+// @updateURL     https://openuserjs.org/meta/sizzle/Show_All_Passwords.meta.js
+// @downloadURL   https://openuserjs.org/install/sizzle/Show_All_Passwords.user.js
+// @match         https://*
+// @match         http://*
+// @match         https://*/*
+// @match         http://*/*
+// @run-at        document-start
+// ==/UserScript==
+
+// Callback function to execute when mutations are observed
+const callback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    if (mutation.type === "childList") {
+      document.querySelectorAll('input[type=password]')
+        .forEach(input => { input.type='text'; });
+    }
+  }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(document, { attributes: false, childList: true, subtree: true });
