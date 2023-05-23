317834d365292de75dabddd0b120293531aa5eca
sizzlemctwizzle <medleymind@gmail.com>
2023-05-22 19:20:16 -0500

    Bugfix

--- Show_All_Passwords.user.js
+++ Show_All_Passwords.user.js
@@ -4,17 +4,18 @@
 // @description   Show all passwords as plain-text. Use at your own risk!
 // @copyright     2023, sizzle (https://openuserjs.org/users/sizzle)
 // @license       MIT
-// @version       1.2
+// @version       1.3
 // @updateURL     https://openuserjs.org/meta/sizzle/Show_All_Passwords.meta.js
 // @downloadURL   https://openuserjs.org/install/sizzle/Show_All_Passwords.user.js
 // @match         https://*
 // @match         http://*
 // @match         https://*/*
 // @match         http://*/*
+// @run-at        document-start
 // ==/UserScript==
 
 const showPasswords = () => document.querySelectorAll('input[type=password]')
-  .forEach(input => input.type = 'text';);
+  .forEach(input => input.type = 'text');
 
 // Callback function to execute when mutations are observed
 const callback = (mutationList, observer) => {
