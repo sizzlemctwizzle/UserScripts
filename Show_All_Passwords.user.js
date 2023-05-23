55cf5de533e9a03ad16f8793f410b8c30e42847f
sizzlemctwizzle <medleymind@gmail.com>
2023-05-22 19:10:40 -0500

    show passwords on page load

--- Show_All_Passwords.user.js
+++ Show_All_Passwords.user.js
@@ -4,22 +4,23 @@
 // @description   Show all passwords as plain-text. Use at your own risk!
 // @copyright     2023, sizzle (https://openuserjs.org/users/sizzle)
 // @license       MIT
-// @version       1.1
+// @version       1.2
 // @updateURL     https://openuserjs.org/meta/sizzle/Show_All_Passwords.meta.js
 // @downloadURL   https://openuserjs.org/install/sizzle/Show_All_Passwords.user.js
 // @match         https://*
 // @match         http://*
 // @match         https://*/*
 // @match         http://*/*
-// @run-at        document-start
 // ==/UserScript==
+
+const showPasswords = () => document.querySelectorAll('input[type=password]')
+  .forEach(input => input.type = 'text';);
 
 // Callback function to execute when mutations are observed
 const callback = (mutationList, observer) => {
   for (const mutation of mutationList) {
     if (mutation.type === "childList") {
-      document.querySelectorAll('input[type=password]')
-        .forEach(input => { input.type='text'; });
+      showPasswords();
     }
   }
 };
@@ -29,3 +30,4 @@ const observer = new MutationObserver(callback);
 
 // Start observing the target node for configured mutations
 observer.observe(document, { attributes: false, childList: true, subtree: true });
+showPasswords();
